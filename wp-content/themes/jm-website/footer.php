951df85597b3e48f90692e83de00a9a47f4dffcf
Akshay Jain <jain.akshay5244@gmail.com>
2019-07-13 10:14:36 +0530

    footer refactoring done

--- wp-content/themes/jm-website/footer.php
+++ wp-content/themes/jm-website/footer.php
@@ -1,54 +1,37 @@
 <footer class="footer">
-	<section class="first-section pt-5">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="wrapper">
-						<a href="<?php echo esc_url(home_url('/')); ?>" class="logo">
-							<img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="">
-						</a>
-						<?php if (has_nav_menu('menu-1')) : ?>
-							<nav id="site-navigation" class="main-navigation navigation py-3"
-							     aria-label="<?php esc_attr_e('Top Menu', 'twentynineteen'); ?>">
-								<?php
-									wp_nav_menu(
-										array(
-											'theme_location' => 'menu-1',
-											'menu_class' => 'main-menu',
-											'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-										)
-									);
-								?>
-							</nav>
-						<?php endif; ?>
-					</div>
-				</div>
-			</div>
-
-		</div>
+	<section class="footer-top">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
+			<img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="">
+		</a>
+		<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
+			<nav id="site-navigation" class="main-navigation navigation py-3"
+			     aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_class'     => 'main-menu',
+							'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+						)
+					);
+				?>
+			</nav>
+		<?php endif; ?>
 	</section>
-	<section class="second-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="wrapper">
-						<?php
-							$q = new WP_Query('pagename=home');
-							while ($q->have_posts()): $q->the_post();
-								?>
-								<div class="copyright">
-									<p><?php echo get_field('copyright'); ?></p>
-								</div>
-								<div class="social-links">
-									<a href="<?php echo get_field('twitter_link'); ?>"><i class="fab fa-twitter"></i></a>
-									<a href="<?php echo get_field('facebook_link'); ?>"><i class="fab fa-facebook-f"></i></a>
-									<a href="<?php echo get_field('linkedin_link'); ?>"><i class="fab fa-linkedin-in"></i></a>
-								</div>
-							<?php endwhile; ?>
-					</div>
+	<section class="footer-bottom">
+		<?php
+			$q = new WP_Query( 'pagename=home' );
+			while ( $q->have_posts() ): $q->the_post();
+				?>
+				<div class="copyright">
+					<p><?php echo get_field( 'copyright' ); ?></p>
 				</div>
-			</div>
-		</div>
+				<div class="social-links">
+					<a href="<?php echo get_field( 'twitter_link' ); ?>"><i class="fab fa-twitter"></i></a>
+					<a href="<?php echo get_field( 'facebook_link' ); ?>"><i class="fab fa-facebook-f"></i></a>
+					<a href="<?php echo get_field( 'linkedin_link' ); ?>"><i class="fab fa-linkedin-in"></i></a>
+				</div>
+			<?php endwhile; ?>
 	</section>
 </footer>
 
@@ -65,8 +48,8 @@
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
 
-<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/glide.min.js"></script>
-<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/main.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/glide.min.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/main.js"></script>
 
 <?php wp_footer(); ?>
 
